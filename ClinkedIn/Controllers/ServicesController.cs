6ba4fd41f5efb3bf4b91abdb683cd9b884800ee6
Michelle Beshears <meeshysu@gmail.com>
2019-04-10 19:09:36 -0500

    merge conflicts

---
+++ ClinkedIn/Controllers/ServicesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClinkedIn.Models
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServicesController : ControllerBase
+    {
+        [HttpPost("services")]
+        public ActionResult AddService(CreateServiceRequest createService)
+        {
+            var newService = ServiceRepository.AddService(createService.Services);
+            return Created($"api/users/{newService.Id}", newService);
+        }
+
+    }
+}
---
+++ ClinkedIn/Models/Services.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClinkedIn.Models
+{
+    public class Services
+    {
+        int Id { get; set; }
+        string Username { get; set; }
+    }
+}
